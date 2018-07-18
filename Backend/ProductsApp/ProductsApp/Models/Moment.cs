218d57f13872241566b52dd752aa369d6e07deb2
ZhaoFan-Su <1015655239@qq.com>
2018-07-19 00:37:23 +0800

    new

---
+++ Backend/ProductsApp/ProductsApp/Models/Moment.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProductsApp.Models
+{
+    public class Moment
+    {
+        public Moment(string id,string sender_id,string content,int likes,int forwards,int collects,int comments,DateTime time)
+        {
+            ID = id;
+            SenderID = sender_id;
+            Content = content;
+            LikeNum = likes;
+            ForwardNum = forwards;
+            CollectNum = collects;
+            CommentNum = comments;
+            Time = time;
+        }
+        public string ID { get; set; }
+        public string SenderID { get; set; }
+        public string Content { get; set; }
+        public int LikeNum { get; set; }
+        public int ForwardNum { get; set; }
+        public int CollectNum { get; set; }
+        public int CommentNum { get; set; }
+        public DateTime Time { get; set; }
+    }
+}
---
+++ Backend/ProductsApp/ProductsApp/Models/Tag.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProductsApp.Models
+{
+    public class Tag
+    {
+        public string Content { get; set; }
+    }
+}
