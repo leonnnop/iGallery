e7944e070bf78154a15bcf7501ff4b3b46629359
SarahWuTX <xlksarah@qq.com>
2018-07-18 17:15:12 +0800

    update

--- Backend/ProductsApp/ProductsApp/App_Start/WebApiConfig.cs
+++ Backend/ProductsApp/ProductsApp/App_Start/WebApiConfig.cs
@@ -18,7 +18,7 @@ namespace ProductsApp
 
             config.Routes.MapHttpRoute(
                 name: "DefaultApi",
-                routeTemplate: "api/{controller}/{id}",
+                routeTemplate: "api/{controller}/{action}",
                 defaults: new { id = RouteParameter.Optional }
             );
         }
