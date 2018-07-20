2d9e993c7cf292c0f119e0d195d9f411baa865ad
SarahWuTX <xlksarah@qq.com>
2018-07-20 18:11:00 +0800

    07/20测试完

--- Backend/ProductsApp/ProductsApp/Controllers/MomentController.cs
+++ Backend/ProductsApp/ProductsApp/Controllers/MomentController.cs
@@ -69,7 +69,6 @@ namespace ProductsApp.Controllers
             return response;
         }
 
-<<<<<<< HEAD
          /// <summary>
         /// 转发动态
         /// </summary>
@@ -80,14 +79,6 @@ namespace ProductsApp.Controllers
         {
             //创建返回信息，先假设插入成功
             int status = 0;
-
-=======
-
-        //搜索与关键词有关动态
-        [HttpGet]
-        public IHttpActionResult Search(string keyword)
-        {
->>>>>>> ce4eabc5e4ead32b92ded52b28511fbef5574f66
             string connStr = @"Data Source=(DESCRIPTION =(ADDRESS_LIST =(ADDRESS = (PROTOCOL = TCP)(HOST = 112.74.55.60)(PORT = 1521)))(CONNECT_DATA =(SERVICE_NAME = orcl)));User Id=vector;Password=Mustafa17";
             OracleConnection conn = new OracleConnection(connStr);
             try
@@ -99,10 +90,10 @@ namespace ProductsApp.Controllers
                 throw (ex);
             }
             OracleCommand cmd = new OracleCommand();
-<<<<<<< HEAD
+
             cmd.Connection = conn;
             cmd.CommandText = "insert into FORWARD(USER_ID,MOMENT_ID) " +
-                    "values('" + forward.User_Id + "','" + forward.Moment_Id + "')";
+                    "values('" + forward.User_ID + "','" + forward.Moment_ID + "')";
             int result = cmd.ExecuteNonQuery();
             if (result != 1)//插入出现错误
             {
@@ -115,37 +106,5 @@ namespace ProductsApp.Controllers
             //返回信息
             return Ok(status);
         }
-
-       
-=======
-            cmd.CommandText = "select MOMENT_ID from Moment_tag where tag like '%" + keyword + "%'";
-            cmd.Connection = conn;
-            OracleDataReader rd = cmd.ExecuteReader();
-            List<Moment> moment_list = new List<Moment>();
-            while (rd.Read())
-            {
-                string moment_id = rd["MOMENT_ID"].ToString();
-                cmd.CommandText = "select * from Moment where ID='" + moment_id + "'";
-                OracleDataReader rd1 = cmd.ExecuteReader();
-                if (rd1.Read())
-                {
-                    string ID = rd1["ID"].ToString();
-                    string Sender_id = rd1["SENGER_ID"].ToString();
-                    string Content = rd1["CONTENE"].ToString();
-                    int Like_num = Convert.ToInt32(rd1["LIKE_NUM"]);
-                    int Forward_num = Convert.ToInt32(rd1["FORWARD_NUM"]);
-                    int Collect_num = Convert.ToInt32(rd1["COLLECT_NUM"]);
-                    int Comment_num = Convert.ToInt32(rd1["COMMENT_NUM"]);
-                    string Time = rd1["SEND_TIME"].ToString();
-                    Moment temp = new Moment(ID, Sender_id, Content, Like_num, Forward_num, Collect_num, Comment_num, Time.ToString());
-                    moment_list.Add(temp);
-                }
-                rd1.Close();
-            }
-            conn.Close();
-            return Json<List<Moment>>(moment_list);
-        }
-        
->>>>>>> ce4eabc5e4ead32b92ded52b28511fbef5574f66
     }
 }
