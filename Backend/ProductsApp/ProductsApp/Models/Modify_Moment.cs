2fa568f491bdefa25234008097b7b02f71452b04
Mustafa <920998500@qq.com>
2018-07-20 21:08:41 +0800

    Mustafa

---
+++ Backend/ProductsApp/ProductsApp/Models/Modify_Moment.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace IGallery.Models
+{
+    public class Modify_Moment
+    {
+        public string email { get; set; }
+        public string moment_id { get; set; }
+        public string content { get; set; }
+    }
+}
