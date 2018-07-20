cebb2d92d97cd68dc70f6c87c68a7108eeca6bb9
ZhaoFan-Su <1015655239@qq.com>
2018-07-20 20:14:55 +0800

    用户关注、取关标签

---
+++ Backend/ProductsApp/ProductsApp/Controllers/Follow_TagController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+using ProductsApp.Models;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace ProductsApp.Controllers
+{
+    public class Follow_TagController : ApiController
+    {
+        DBAccess Access = new DBAccess();
+        [HttpPut]
+        public HttpResponseMessage FollowTag(string UserId, string tag)
+        {
+           HttpResponseMessage response = Request.CreateResponse();
+           if (Access.ExecuteSql(Access.Insert("FOLLOW_TAG", "'" + UserId + "', '" + tag + "'")))
+           {
+               response.StatusCode = HttpStatusCode.OK;
+               response.Content = new StringContent("关注成功", Encoding.Unicode);
+           }
+           else
+           {
+               response.StatusCode = HttpStatusCode.Forbidden;
+               response.Content = new StringContent("关注失败", Encoding.Unicode);
+           }
+           return response;
+        }
+
+        [HttpPut]
+        public HttpResponseMessage CancleFollowTag(string UserId, string tag)
+        {
+            HttpResponseMessage response = Request.CreateResponse();
+            if (Access.ExecuteSql(Access.Delete("FOLLOW_TAG", "USER_ID= '" + UserId + "' and TAG= '" + tag + "'")))
+            {
+                response.StatusCode = HttpStatusCode.OK;
+                response.Content = new StringContent("取关成功", Encoding.Unicode);
+            }
+            else
+            {
+                response.StatusCode = HttpStatusCode.Forbidden;
+                response.Content = new StringContent("取关失败", Encoding.Unicode);
+            }
+            return response;
+        }
+    }
+}
