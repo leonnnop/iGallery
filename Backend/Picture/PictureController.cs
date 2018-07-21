06d8588e4a0cfa5f4410de56c6b277537b5f0038
100101001 <17702113437@163.com>
2018-07-21 20:30:47 +0800

    Add files via upload

---
+++ Backend/Picture/PictureController.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Utility;
+using Newtonsoft.Json;
+using System.Net.Http.Headers;
+using System.Net.Http.Formatting;
+using System.Web;
+using Newtonsoft.Json.Linq;
+using System.Text;
+using ProductsApp.Models;
+
+namespace ProductsApp.Controllers
+{
+   
+    public class PictureController : ApiController
+    {
+
+        [HttpGet]
+        public HttpResponseMessage FirstGet(string id,int type)
+        {
+
+            HttpResponseMessage result = new HttpResponseMessage();
+            
+            if (type == 2)
+            {
+                result.StatusCode = HttpStatusCode.OK;
+                Tuple<ByteArrayContent, string> res = Util.Get(id, 2);
+                result.Content=res.Item1; //头图返图
+                string ext = res.Item2;   //格式解析
+                result.Content.Headers.ContentType = new MediaTypeHeaderValue("image/"+ext.Substring(1,ext.Length-1));
+            }
+            else
+            {
+                List<string> list = Util.GetPid(id);
+                result.Content = new StringContent(JsonConvert.SerializeObject(list), Encoding.GetEncoding("UTF-8"), "application/json"); //非头图返文件号
+                
+            }
+            return result ;
+        }
+
+        [HttpGet]
+        public HttpResponseMessage Gets(string pid)
+        {
+            HttpResponseMessage result = new HttpResponseMessage();
+            Tuple<ByteArrayContent, string> res = Util.Get(pid, 1);
+            result.Content = res.Item1; //动态组图返图pid
+            string ext = res.Item2;   //格式解析
+            result.Content.Headers.ContentType = new MediaTypeHeaderValue("image/" + ext.Substring(1, ext.Length - 1));
+            return result;
+        }
+
+        [HttpPost]
+        public HttpResponseMessage Save()
+        {
+            //从{富参}表单获取参数
+            var data=HttpContext.Current.Request.Params;
+            string id = data["id"];
+            int type = Convert.ToInt32(data["type"]);
+
+            HttpResponseMessage result = new HttpResponseMessage();
+
+            if(Util.Post(id, type))
+            {
+                result.StatusCode = HttpStatusCode.OK;
+            }
+            else
+            {
+                result.StatusCode = HttpStatusCode.InternalServerError;
+            }
+            return result;
+        }
+    }
+}
---
+++ Backend/Picture/Util.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Web;
+using Oracle.ManagedDataAccess.Client;
+namespace Utility
+{
+    public class Util
+    {
+        // GET api/values/5  
+        //给出用户id或动态id
+
+        public static Tuple<ByteArrayContent,string> Get(string id,int type)
+        {
+            //打开数据库
+            string connStr = @"Data Source=(DESCRIPTION =(ADDRESS_LIST =(ADDRESS = (PROTOCOL = TCP)(HOST = 112.74.55.60)(PORT = 1521)))(CONNECT_DATA =(SERVICE_NAME = orcl)));User Id=vector;Password=Mustafa17";
+            OracleConnection conn = new OracleConnection(connStr);
+            conn.Open();
+            OracleCommand cmd = new OracleCommand();
+            cmd.Connection = conn;
+
+
+            //取图片全路径
+            if (type == 1)
+            {
+                cmd.CommandText = "select URL from PICTURE where ID='" + id + "'";  //多路径
+            }
+            else if (type == 2)
+            {
+                cmd.CommandText = "select PHOTO from USERS where EMAIL='" + id + "'";
+            }
+            OracleDataReader rd = cmd.ExecuteReader();
+
+            rd.Read();
+            string filePath = rd[0].ToString();
+
+            //本地读文件
+            if (filePath.Trim().Equals(""))
+            {
+                return null;
+            }
+            Image img = Image.FromFile(filePath);  //这里我把路径给出了，他只用给我文件名
+
+
+            //图片文件转字节流
+            MemoryStream ms = new MemoryStream();
+
+
+            //分割后缀
+            string fileExt = Path.GetExtension(filePath);
+
+            //判断后缀
+            ImageFormat format = null;
+            switch (fileExt.ToLower())
+            {
+                case ".png":
+                    format = ImageFormat.Png;
+                    break;
+                case ".bmp":
+                    format = ImageFormat.Bmp;
+                    break;
+                case ".jpeg":
+                    format = ImageFormat.Jpeg;
+                    break;
+                case ".jpg":
+                    format = ImageFormat.Jpeg;
+                    break;
+            }
+
+            //从格式图像转成字节流
+            img.Save(ms, format);
+            rd.Close();
+            conn.Close();
+
+            //字节流转回前端字节数组
+            return new Tuple<ByteArrayContent,string>(new ByteArrayContent(ms.ToArray()),fileExt); //字节流转数组
+
+        }
+
+
+        public static List<string> GetPid(string Mid)
+        {
+            List<String> list = new List<String>();
+            //开库
+            string connStr = @"Data Source=(DESCRIPTION =(ADDRESS_LIST =(ADDRESS = (PROTOCOL = TCP)(HOST = 112.74.55.60)(PORT = 1521)))(CONNECT_DATA =(SERVICE_NAME = orcl)));User Id=vector;Password=Mustafa17";
+            OracleConnection conn = new OracleConnection(connStr);
+            conn.Open();
+
+            //
+            OracleCommand cmd = new OracleCommand();
+            cmd.Connection = conn;
+            cmd.CommandText = "select ID from PICTURE where MOMENT_ID='" + Mid + "' ";
+
+            OracleDataReader rd = cmd.ExecuteReader();
+            while (rd.Read())
+            {
+                list.Add(rd[0].ToString());
+            }
+
+            rd.Close();
+            conn.Close();
+
+            return list;
+      
+        }
+
+        //post api/values  
+        //动态加组图（动态ID）or 个人信息修改加头图（用户ID） 
+        public static bool Post(string id,int type)
+        {
+            bool flag = true;
+           
+            //打开数据库
+            string connStr = @"Data Source=(DESCRIPTION =(ADDRESS_LIST =(ADDRESS = (PROTOCOL = TCP)(HOST = 112.74.55.60)(PORT = 1521)))(CONNECT_DATA =(SERVICE_NAME = orcl)));User Id=vector;Password=Mustafa17";
+            OracleConnection conn = new OracleConnection(connStr);
+            conn.Open();
+            OracleCommand cmd = new OracleCommand();
+            cmd.Connection = conn;
+            //浏览器上传文件的body
+            var SrcRequest = HttpContext.Current.Request;
+
+
+            string filepath = "";
+            //逐一存下body中给出的文件集合
+            foreach (string f in SrcRequest.Files.AllKeys)
+            {
+                HttpPostedFile file = SrcRequest.Files[f];
+                //分割后缀
+                string fileExt = Path.GetExtension(file.FileName);
+
+                if (type == 1)
+                {
+                    //插入PICTURE一条记录
+                    cmd.CommandText = "select count(*) from PICTURE";
+                    int pid = Convert.ToInt32(cmd.ExecuteScalar()) + 1;
+                    //文件路径不重名
+                    filepath = @".\mmps\" + (Convert.ToString(pid)) + fileExt;
+                    cmd.CommandText = "insert into PICTURE(ID,URL,MOMENT_ID) values('" + Convert.ToString(pid) + "','" + filepath + "','" + id + "')";
+                    if (cmd.ExecuteNonQuery() != 1)
+                    {
+                       flag = false;
+                    }
+                }
+                else if (type == 2)
+                {
+                    //更新用户个人头像
+                    filepath = @".\heads\" + id + fileExt;
+                    cmd.CommandText = "update USERS set PHOTO='" + filepath + "' where EMAIL='" + id + "'";
+                    if (cmd.ExecuteNonQuery() != 1)
+                    {
+                        flag = false;
+                    }
+                }
+
+                //存本地
+                file.SaveAs(filepath);
+            }
+            return flag;
+        }
+
+    }
+
+}
+
