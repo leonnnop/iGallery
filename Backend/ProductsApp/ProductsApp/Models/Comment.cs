c44fc9f20b7d989e34c67300ebcf1e0bd73956c0
SarahWuTX <xlksarah@qq.com>
2018-07-22 16:57:26 +0800

    xx

--- Backend/ProductsApp/ProductsApp/Models/Comment.cs
+++ Backend/ProductsApp/ProductsApp/Models/Comment.cs
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -20,27 +19,4 @@ namespace ProductsApp.Models
         public string Quote_content { get; set; }
         
     }
-=======
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-
-namespace ProductsApp.Models
-{
-    public class Comment
-    {
-        public string Mid { get; set; }
-        public string Sender_id { get; set; }
-        public string Sender_username { get; set; }
-        public string Cid { get; set; }
-        public string Content { get; set; }
-        public string Quote_username { get; set; }
-        public string Send_time { get; set; }
-        public string Quote_id { get; set; }
-        public string Type { get; set; }
-        public string Quote_content { get; set; }
-        
-    }
->>>>>>> 96f694c8f0ba26cc2f78da25590696cc87e26b98
 }
